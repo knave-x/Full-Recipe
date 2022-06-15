f9541986f4da734b8cc2b7dad819dc7cf8837f9d
batuhan aydin <baydin@nuevo.com.tr>
2022-06-15 16:35:21 +0300

    ++

---
+++ src/components/Category.jsx
@@ -0,0 +1,19 @@
+import { FaPizzaSlice, FHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopSticks } from "react-icons/gi";
+import styled from "styled-components";
+//import { NavLink } from "react-router-dom";
+
+import React from "react";
+
+const Category = () => {
+  return (
+    <div>
+      <div>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
