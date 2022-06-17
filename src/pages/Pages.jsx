2177d86d7374154358445882ed93088303562cd1
knave-x <59017636+knave-x@users.noreply.github.com>
2022-06-17 12:49:14 +0300

    Update pages

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 
 const Pages = () => {
   return (
@@ -9,6 +10,9 @@ const Pages = () => {
       <Route path="/" element={<Home />} />
       <Route path="cuisine">
         <Route path=":type" element={<Cuisine />} />
+      </Route>
+      <Route path="searched" >
+        <Route path=":search" element={<Searched/>} /> 
       </Route>
     </Routes>
   );
