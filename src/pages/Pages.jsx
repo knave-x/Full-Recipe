8381864aa6731aa3a3dea87b223bfa652fd3afb5
batuhan aydin <baydin@nuevo.com.tr>
2022-06-20 12:15:17 +0300

    add motion

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 const Pages = () => {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="cuisine">
-        <Route path=":type" element={<Cuisine />} />
-      </Route>
+    <AnimatePresence exitBeforeEnter>
+      <Routes Location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="cuisine">
+          <Route path=":type" element={<Cuisine />} />
+        </Route>
 
-      <Route path="searched">
-        <Route path=":search" element={<Searched />} />
-      </Route>
+        <Route path="searched">
+          <Route path=":search" element={<Searched />} />
+        </Route>
 
-      <Route path="recipe">
-        <Route path=":name" element={<Recipe />} />
-      </Route>
-    </Routes>
+        <Route path="recipe">
+          <Route path=":name" element={<Recipe />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 };
 
